7cd778bba79e99623d4b65b9611ae42e533f2614
Fahad Aijaz <fahad.aijaz4@gmail.com>
2020-11-06 01:46:17 +0500

    changed

--- TOTP/Program.cs
+++ TOTP/Program.cs
@@ -7,11 +7,23 @@ namespace TOTP
     {
         static void Main(string[] args)
         {
-            var secretKey = Encoding.ASCII.GetBytes("abc");
+            var secretKey = Base32Encoding.ToBytes("NBSWY3DP");
+            Console.Write(Encoding.ASCII.GetString(secretKey));
             var totpStep = 30;
             var totpSize = 8;
             var totp = new Totp(secretKey, mode: OtpHashMode.Sha256, step:totpStep, totpSize: totpSize);
-            Console.WriteLine(totp.ComputeTotp(DateTime.Now));
+
+            System.Timers.Timer timer = new System.Timers.Timer();
+            timer.Interval = 5000;
+            timer.Elapsed += timer_Elapsed;
+            timer.Start();
+            Console.ReadKey();
+            void timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+            {
+                var now = DateTime.Now.ToUniversalTime();
+                Console.WriteLine(now + " " + totp.ComputeTotp(now));
+            }
+
         }
     }
 }
