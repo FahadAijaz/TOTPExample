f2f5f4f5f2dfea255ebdba3aba00b7d5dbb35139
FahadAijaz <fahad.aijaz4@gmail.com>
2020-11-06 00:15:37 +0500

    totp made

---
+++ TOTP/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text;
+using OtpNet;
+namespace TOTP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var secretKey = Encoding.ASCII.GetBytes("abc");
+            var totpStep = 30;
+            var totpSize = 8;
+            var totp = new Totp(secretKey, mode: OtpHashMode.Sha256, step:totpStep, totpSize: totpSize);
+            Console.WriteLine(totp.ComputeTotp(DateTime.Now));
+        }
+    }
+}
